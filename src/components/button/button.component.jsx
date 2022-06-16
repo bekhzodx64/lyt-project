b673ca5e27d9958075728ea80c42f0511762be51
bekhzodx64 <mr.bekzod2016@mail.ru>
2022-06-17 00:28:15 +0500

    preparing search popup

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,6 +1,7 @@
 const BUTTON_TYPES_CLASSES = {
 	main: 'main',
 	inverted: 'inverted',
+	search: 'search',
 };
 
 const Button = ({ children, buttonType, ...otherProps }) => {
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,15 @@
+import { AiOutlineEdit, AiOutlineLogin } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-import { CgMenuGridO } from 'react-icons/cg';
 import Button from '../button/button.component';
-import { AiOutlineLogin, AiOutlineEdit } from 'react-icons/ai';
+import SearchPopup from '../search-popup/searchPopup.component';
 
 const Header = () => {
 	return (
 		<header className='shadow bg-white'>
-			<div className='container py-4 flex justify-between sm:grid grid-cols-3 items-center'>
-				<button type='button' className='menu-button group'>
-					<CgMenuGridO className='group-hover:text-white' />
+			<div className='container py-4 flex justify-between sm:grid grid-cols-3 items-center relative'>
+				<button type='button' className='search-button group'>
+					<BiSearch className='group-hover:text-white' />
 				</button>
 				<div className='justify-self-center'>
 					<Link to='/' tabIndex='-1'>
@@ -18,7 +19,7 @@ const Header = () => {
 						</div>
 					</Link>
 				</div>
-				<div className='justify-self-end space-x-1 sm:space-x-3'>
+				<div className='justify-self-end space-x-3'>
 					<Button buttonType='inverted'>
 						<AiOutlineEdit className='md:hidden' />
 						<span className='hidden md:block'>Sign in</span>
@@ -28,6 +29,7 @@ const Header = () => {
 						<span className='hidden md:block'>Sing up</span>
 					</Button>
 				</div>
+				<SearchPopup />
 			</div>
 		</header>
 	);
