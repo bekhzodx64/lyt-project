0530a4f74b03a914b0d634c066fe788535a5c53a
bekhzodx64 <mr.bekzod2016@mail.ru>
2022-06-21 12:52:49 +0500

    bug fixed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,9 +5,9 @@ import UserCard from '../../components/user-card/user-card.component';
 const Home = ({ users }) => {
 	return (
 		<div className='space-y-5 py-20 container'>
-			<div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 place-items-center gap-10 pt-5'>
+			<div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-items-center gap-10 pt-5'>
 				{users
-					.filter((_, idx) => idx < 5)
+					.filter((_, idx) => idx < 4)
 					.map((user) => (
 						<UserCard user={user} key={user.id} />
 					))}
